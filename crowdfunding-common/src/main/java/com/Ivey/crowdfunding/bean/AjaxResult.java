727cf77cc67c708a25da94d2775e2053fd06b14f
Ivey <906572535@qq.com>
2020-01-21 21:43:28 +0800

    完成登录功能

---
+++ crowdfunding-common/src/main/java/com/Ivey/crowdfunding/bean/AjaxResult.java
@@ -0,0 +1,20 @@
+package com.Ivey.crowdfunding.bean;
+
+/**
+ * @Description
+ * @Author IveyLv
+ * @Date 2020/1/21 21:01
+ * @Version 1.0
+ */
+public class AjaxResult {
+
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
